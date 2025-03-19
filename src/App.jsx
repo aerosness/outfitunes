3e0530d375c11acd8f6314ab242990bc1ee70028
Semyon Tyo <78652129+aerosness@users.noreply.github.com>
2025-03-19 21:38:08 +0500

    df

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import "./App.css";
-import NavigationBar from "./components/NavigationBar";
 import WebApp from "./pages/WebApp";
 
 const App = () => {
   return (
     <Router>
-      <NavigationBar />
       <Routes>
         <Route
           path={ROUTES.HOME}
