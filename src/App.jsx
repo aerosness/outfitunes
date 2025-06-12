f6f47ccb6a343d7eefe299fc4533f4831d9391a5
Xeon_Fox <78652129+aerosness@users.noreply.github.com>
2025-06-12 19:54:55 +0500

    hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import * as ROUTES from "./constants/routes";
 import Registration from "./pages/Registration/Registration";
 import Playlists from "./pages/Playlists/Playlists";
